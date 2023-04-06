c692d5f038537a22c06667d56d8a80b2f8b18f5c
Youssef Muhamad <me.youssefmuhamad@gmail.com>
2023-04-05 23:05:53 -0300

    config: install expo-camera, expo-face-detector and reanimated

---
+++ src/Home/index.tsx
@@ -0,0 +1,11 @@
+import { View } from 'react-native';
+import { styles } from './styles';
+import { Camera } from 'expo-camera';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+        <Camera style={styles.camera}/>
+    </View> 
+  );
+}
