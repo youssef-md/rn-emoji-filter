8134e6c1030411cc41c2c6a5a8f15f92f3803954
Youssef Muhamad <me.youssefmuhamad@gmail.com>
2023-04-05 23:31:32 -0300

    feat: animate a box on the user face

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,10 +1,51 @@
+import { useEffect, useState } from 'react';
 import { View } from 'react-native';
 import { styles } from './styles';
-import { Camera, CameraType } from 'expo-camera';
-import { useEffect } from 'react';
+import { Camera, CameraType, FaceDetectionResult } from 'expo-camera';
+import * as FaceDetector from 'expo-face-detector';
+import Animated, { useSharedValue, useAnimatedStyle } from 'react-native-reanimated'
 
 export function Home() {
   const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [faceDetected, setFaceDetected] = useState(false);
+
+  const faceValues = useSharedValue({
+    width: 0,
+    height: 0,
+    x: 0,
+    y: 0,
+  });
+
+  function handleFaceDetection({ faces }: FaceDetectionResult) {
+    const face = faces[0] as any;
+
+    if (face) {
+      const { size: { width, height }, origin: { x, y } } = face.bounds;
+      faceValues.value = {
+        width, height,
+        x, y
+      }
+
+      setFaceDetected(true);
+    } else {
+      setFaceDetected(false);
+    }
+
+
+  }
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    position: 'absolute',
+    zIndex: 1,
+    width: faceValues.value.width,
+    height: faceValues.value.height,
+    transform: [
+      { translateX: faceValues.value.x },
+      { translateY: faceValues.value.y },
+    ],
+    borderColor: 'blue',
+    borderWidth: 10
+  }))
 
   useEffect(() => {
     requestPermission()
@@ -16,7 +57,19 @@ export function Home() {
 
   return (
     <View style={styles.container}>
-      <Camera style={styles.camera} type={CameraType.front} />
+      {faceDetected && <Animated.View style={animatedStyle} />}
+      <Camera
+        style={styles.camera}
+        type={CameraType.front}
+        onFacesDetected={handleFaceDetection}
+        faceDetectorSettings={{
+          mode: FaceDetector.FaceDetectorMode.fast,
+          detectLandmarks: FaceDetector.FaceDetectorLandmarks.all,
+          runClassifications: FaceDetector.FaceDetectorClassifications.all,
+          minDetectionInterval: 100,
+          tracking: true,
+        }}
+      />
     </View>
   );
 }
