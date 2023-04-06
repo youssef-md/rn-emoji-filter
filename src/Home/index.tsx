48235f473e997deb6a350f7b7bcd282986869ee1
Youssef Muhamad <me.youssefmuhamad@gmail.com>
2023-04-05 23:58:54 -0300

    feat: render the correct emoji based on expression probability

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,36 +1,51 @@
 import { useEffect, useState } from 'react';
-import { View } from 'react-native';
-import { styles } from './styles';
+import { ImageSourcePropType, View } from 'react-native';
+
 import { Camera, CameraType, FaceDetectionResult } from 'expo-camera';
 import * as FaceDetector from 'expo-face-detector';
 import Animated, { useSharedValue, useAnimatedStyle } from 'react-native-reanimated'
 
+import { styles } from './styles';
+import neutralEmoji from '../assets/neutral.png';
+import smilingEmoji from '../assets/smiling.png';
+import smilingHardEmoji from '../assets/smiling-hard.png';
+import winkingEmoji from '../assets/winking.png';
+
 export function Home() {
   const [permission, requestPermission] = Camera.useCameraPermissions();
   const [faceDetected, setFaceDetected] = useState(false);
+  const [emoji, setEmoji] = useState<ImageSourcePropType>(neutralEmoji);
 
-  const faceValues = useSharedValue({
-    width: 0,
-    height: 0,
-    x: 0,
-    y: 0,
-  });
+  const faceValues = useSharedValue({ width: 0, height: 0, x: 0, y: 0 });
 
   function handleFaceDetection({ faces }: FaceDetectionResult) {
     const face = faces[0] as any;
+    const hasFace = !!face;
 
-    if (face) {
-      const { size: { width, height }, origin: { x, y } } = face.bounds;
-      faceValues.value = {
-        width, height,
-        x, y
-      }
+    setFaceDetected(hasFace);
+    if (!hasFace) return;
 
-      setFaceDetected(true);
-    } else {
-      setFaceDetected(false);
+    const { size: { width, height }, origin: { x, y } } = face.bounds;
+    faceValues.value = {
+      width, height,
+      x, y
     }
 
+    const isSmiling = face.smilingProbability > 0.5;
+    const isLeftEyeClosed = face.leftEyeOpenProbability < 0.5;
+    const isRightEyeClosed = face.rightEyeOpenProbability < 0.5;
+    const isRightEyeOpen = face.rightEyeOpenProbability > 0.5;
+    const isWinking = isLeftEyeClosed && isRightEyeOpen;
+    const isAnyEyesClosed = isLeftEyeClosed || isRightEyeClosed;
+
+    if (isSmiling && !isAnyEyesClosed) {
+      setEmoji(smilingHardEmoji);
+    }
+    else if (isWinking) {
+      setEmoji(winkingEmoji);
+    } else {
+      setEmoji(neutralEmoji);
+    }
 
   }
 
@@ -43,8 +58,6 @@ export function Home() {
       { translateX: faceValues.value.x },
       { translateY: faceValues.value.y },
     ],
-    borderColor: 'blue',
-    borderWidth: 10
   }))
 
   useEffect(() => {
@@ -57,7 +70,7 @@ export function Home() {
 
   return (
     <View style={styles.container}>
-      {faceDetected && <Animated.View style={animatedStyle} />}
+      {faceDetected && <Animated.Image style={animatedStyle} source={emoji} />}
       <Camera
         style={styles.camera}
         type={CameraType.front}
